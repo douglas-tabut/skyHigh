2122b9d312759766bf24734cf7d9687e86b6061d
Derrick Ampire <ampire90@gmail.com>
2023-11-16 15:18:08 +0300

    max api call limit added and tests excluded

--- data-scrapper/scrapper.py
+++ data-scrapper/scrapper.py
@@ -6,9 +6,6 @@ The fetched snippets are then saved to a cached file.
 
 import requests
 import json
-
-
-# get GitHub personal access token saved as an environment variable
 from dotenv import load_dotenv
 import os
 
@@ -21,16 +18,26 @@ repositories = [
         {'owner': 'psf', 'repo': 'requests'},
         {'owner': 'numpy', 'repo': 'numpy'},
         {'owner': 'django', 'repo': 'django'},
-        {'owner': 'pandas-dev', 'repo': 'pandas'},
-        {'owner': 'scikit-learn', 'repo': 'scikit-learn'},
-        {'owner': 'pallets', 'repo': 'flask'},
-        {'owner': 'psf', 'repo': 'requests-html'},
-        {'owner': 'pytorch', 'repo': 'pytorch'},
-        {'owner': 'celery', 'repo': 'celery'},
-        {'owner': 'matplotlib', 'repo': 'matplotlib'}
+        # {'owner': 'pandas-dev', 'repo': 'pandas'},
+        # {'owner': 'scikit-learn', 'repo': 'scikit-learn'},
+        # {'owner': 'pallets', 'repo': 'flask'},
+        # {'owner': 'psf', 'repo': 'requests-html'},
+        # {'owner': 'pytorch', 'repo': 'pytorch'},
+        # {'owner': 'celery', 'repo': 'celery'},
+        # {'owner': 'matplotlib', 'repo': 'matplotlib'}
 ]
 
 CACHE_FILE = 'cached_code_snippets.json'
+MAX_SNIPPETS_LIMIT = 350
+
+TEST_FILE_TERMS = ['test', 'tests'] # Common terms used in test file names
+
+def is_test_file(file_name):
+    """
+    Checks if the file is a test file based on the name.
+    """
+    return any(term in file_name.lower() for term in TEST_FILE_TERMS)
+
 
 def fetch_python_code_snippets(owner, repo, path=''):
     """
@@ -47,8 +54,12 @@ def fetch_python_code_snippets(owner, repo, path=''):
 
         # To-Do: Implement parallelizarion instead of caching using concurrent.futures.ThreadPoolExecutir()
         for content in contents:
-            if content['type'] == 'file' and content['name'].endswith('.py'):
-                # Fetch the content of Python files
+            if (content['type'] == 'file' and
+                content['name'].endswith('.py') and
+                content['name'] != '__init__.py' and
+                not is_test_file(content['name'])):
+
+                # Fetch the content of Python files and exclude the __init__.py and test files
                 file_url = content['download_url']
                 file_content = requests.get(file_url).text
                 python_code_snippets.append({
@@ -58,18 +69,15 @@ def fetch_python_code_snippets(owner, repo, path=''):
                 })
                 print(f"Fetched snippet: {content['name']} from {owner}/{repo}")
 
+                if len(python_code_snippets) >= MAX_SNIPPETS_LIMIT:
+                    break
+
             elif content['type'] == 'dir':
                 # Recursively fetch code snippets from files within folders
                 folder_path = f"{path}/{content['name']}"
                 snippets = fetch_python_code_snippets(owner, repo, folder_path)
                 if snippets:
                     python_code_snippets.extend(snippets)
-
-        # Save the fetched code snippets to a caech file
-        with open(CACHE_FILE, 'a') as cache_file:
-            for snippet in python_code_snippets:
-                json.dump(snippet, cache_file)
-                cache_file.write('\n')
 
         return python_code_snippets
     else:
@@ -78,22 +86,24 @@ def fetch_python_code_snippets(owner, repo, path=''):
 
 
 if __name__ == '__main__':
-    # Check if cache file exists otherwise create it
-    if not os.path.exists(CACHE_FILE):
-        with open(CACHE_FILE, 'w') as cache_file:
-            cache_file.write('[') # initialize the JSON array
-
     all_snippets = []
 
-    for repo_info in repositories:
-        owner, repo = repo_info['owner'], repo_info['repo']
-        snippets = fetch_python_code_snippets(owner, repo)
+    try:
+        for repo_info in repositories:
+            owner, repo = repo_info['owner'], repo_info['repo']
+            snippets = fetch_python_code_snippets(owner, repo)
 
-        if snippets:
-            all_snippets.extend(snippets)
+            if snippets:
+                all_snippets.extend(snippets)
+
+            if len(all_snippets) >= MAX_SNIPPETS_LIMIT:
+                    break  # Exit the loop when the overall limit is reached
+
+    except KeyboardInterrupt:
+        pass  # Catch Ctrl+C and proceed to saving snippets
 
     # Save code snippets to a JSON array in the cache file
-    with open(CACHE_FILE, 'a') as cache_file:
-        cache_file.write(']')
+    with open(CACHE_FILE, 'w') as cache_file:
+        json.dump(all_snippets, cache_file, indent=4)
 
     print('Code snippets saved to cached_code_snippets.json')
