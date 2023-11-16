c54ec0b1a0f4278bf2465d6e95a070ce888c8efc
Derrick Ampire <ampire90@gmail.com>
2023-11-16 11:26:08 +0300

    modified to add decompressor function

--- data-scrapper/compressor.py
+++ data-scrapper/compressor.py
@@ -1,23 +1,40 @@
 """
-This module compresses the `cached_code_snippets.json` file
-into a zip so it can be pushed to a GitHub repo.
+This module contains functions for compressing a json file to a .gz
+file and decompressing from .gz to a json file.
 """
 
 import json
 import gzip
 import os
 
+
+def compress_json_to_gzip(input_file, output_file):
+    """Compresses a JSON file to a gzip file."""
+    with open(input_file, 'r', encoding='utf-8') as json_file:
+        content = json_file.read()
+
+    with gzip.open(output_file, 'wb') as compressed_file:
+        compressed_file.write(content.encode('utf-8'))
+
+def decompress_gzip_to_json(input_file, output_file):
+    """Decompresses a gzip file to a JSON file."""
+    with gzip.open(input_file, 'rt', encoding='utf-8') as compressed_file:
+        content = compressed_file.read()
+
+    with open(output_file, 'w', encoding='utf-8') as json_file:
+        json_file.write(content)
+
 CACHE_FILE = 'cached_code_snippets.json'
 COMPRESSED_CACHE_FILE = 'cached_code_snippets.json.gz'
 
-# Load the existing cache file
-with open(CACHE_FILE, 'r') as file:
-    content = file.read()
-
-# Compress the content and save it to a new file
-with gzip.open(COMPRESSED_CACHE_FILE, 'wb') as compressed_file:
-    compressed_file.write(content.encode('utf-8'))
+"""
+Comment this code out in case you want to compress and comment the decompressor
+# Compress JSON to Gzip
+compress_json_to_gzip(CACHE_FILE, COMPRESSED_CACHE_FILE)
 
 print(f'Original file size: {os.path.getsize(CACHE_FILE)} bytes')
 print(f'Compressed file size: {os.path.getsize(COMPRESSED_CACHE_FILE)} bytes')
-
+"""
+# Decompress Gzip to JSON
+DECOMPRESSED_CACHE_FILE = 'decompressed_code_snippets.json'
+decompress_gzip_to_json(COMPRESSED_CACHE_FILE, DECOMPRESSED_CACHE_FILE)
