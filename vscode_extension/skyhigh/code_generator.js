ebba6def317f9ea932bc93afa3af8bf530f3d8e0
douglas-tabut <tabutdouglas@gmail.com>
2023-11-17 16:01:49 +0300

    update from douglas:sam huddle

--- vscode_extension/skyhigh/code_generator.js
+++ vscode_extension/skyhigh/code_generator.js
@@ -2,7 +2,7 @@ const {CohereClient} = require('cohere-ai');
 
 // userSnippetStart variable contains what the person has started typing , then it is fed into the model to be autocompleted
 
-let userSnippetStart = 'def reverse_string()'; 
+let userSnippetStart = 'import'; 
 
 
 const PROMPT = `You are a helpful code assistant that helps in writing useful code snippets in python. Your language of choice is Python. Don't explain the code, just generate the code block itself
@@ -20,9 +20,9 @@ async function code_generator() {
         maxTokens: 300,
     });
     
-    let output = prediction.generations[0].text;
-    console.log(ouput)
+    let output = prediction.generations[0];
+    // const new_json = JSON.parse('"{'+output+'}')
+    console.log(output)
     return output;
 };
-
-export default code_generator;
+code_generator()
